1607eade2a500ec7e642a2500ec5543fa5e47ae8
divyansh9557 <sainids019@gmail.com>
2025-05-11 16:42:01 +0530

    params change

--- src/app/course-details/[id]/page.tsx
+++ src/app/course-details/[id]/page.tsx
@@ -2,12 +2,15 @@
 
 import CourseDetailss from './courseDetails';
 
-const CourseDetails = async({params}:{params:Promise<{id:string}>}) => {
-  const {id}= await params
-return (<>
-<CourseDetailss id={ id} />;
+const CourseDetails = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
 
-</>)
+  return (
+    <>
+      <CourseDetailss id={id} />
+    </>
+  );
 };
 
 export default CourseDetails;
+
